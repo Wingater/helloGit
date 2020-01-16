12a41859b76d7adecbf5982d19685cf62dca5ee7
Wingater <Wingater@LAPTOP-65HNGKVS>
2020-01-16 19:36:47 +0900

    repository

--- 02_HelloGit/src/com/kh/git/HelloGitTest.java
+++ 02_HelloGit/src/com/kh/git/HelloGitTest.java
@@ -5,6 +5,6 @@ public class HelloGitTest {
 	public static void main(String[] args) {
 		System.out.println("안녕하세요!");
 		System.out.println("맛저?!");
-		System.out.println("축하드립니당!");
+		System.out.println("축하드립니당!!");
 	}
 }
